40d58a7b84cff46095cfa24317fad26cb589fddf
ximenareza <xim3naacosta.22@gmail.com>
2024-09-25 10:01:38 -0600

    FEAT: Completo

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# carrito
+<center>
+    <img src="./md/images/ITGAMBanner.png" alt="Itgam Banner">
+</center>
+
+# Shopping Cart 🛒 Project
+
+Proyecto didactico para el curso de
+Desarrollo de apps en dispositivos móviles
+
+## Autor
+
+Ivan Rivalcoba 🐦‍🔥
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+  import { ref } from 'vue';
+  // Modelo
+  const header = ref('App lista de compras');
+  const items = ref([
+    {id:'0', label: '10 bolillos'},
+    {id:'1', label:'1 lata frijoles'},
+    {id:'2', label:'1 Chelas'},
+    {id:'3', label:'1 Nutella'},
+  ]);
+</script>
+
+<template>
+  <h1>
+    <i class="material-icons shopping-cart-icon">local_mall</i>
+    {{ header }}
+  </h1>
+  <ul>
+    <li v-for="item in items" :key="item.id"> 🛍️ {{ item.label }} </li>
+  </ul>
+</template>
+
+<style scoped>
+.shopping-cart-icon {
+  font-size: 2rem;
+}
+</style>
