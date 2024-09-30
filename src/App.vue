19b8c6c862e28f4f91a19ee15632f05688608385
ximenareza <xim3naacosta.22@gmail.com>
2024-09-30 12:04:51 -0600

    ⭐FEAT: Reto

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,45 @@
 <script setup>
-  import { ref } from 'vue';
-  // Modelo
-  const header = ref('App lista de compras');
-  const items = ref([
-    {id:'0', label: '10 bolillos'},
-    {id:'1', label:'1 lata frijoles'},
-    {id:'2', label:'1 Chelas'},
-    {id:'3', label:'1 Nutella'},
-  ]);
-  const newItem = ref('');
-  const newItemPriority = ref('low');
-</script>
+import { ref } from 'vue';
+// Modelo
+const header = ref('App lista de compras');
+const items = ref([
+  {id:'0', label: '10 bolillos'},
+  {id:'1', label: '1 crema de litro'},
+  {id:'2', label: '1/4 de jamon'},
+  {id:'3', label: '1 nutella'}
+]);
+const newItem = ref("");
+const newItemHighPriority = ref(false);
 
+</script>
+ 
 <template>
   <h1>
-    <i class="material-icons shopping-cart-icon">local_mall</i>
+   <i class="material-icons shopping-cart-icon">local_mall</i>
     {{ header }}
   </h1>
-  <input v-model="newItem" type="text" placeholder="Agregar Articulo"> 
-  <!--RADIO BUTTOS-->
+  <form v-on:submit.prevent="items.push({id: items.length + 1, label: newItem})" class="add-item form">
+  <input v-model="newItem" type="text" placeholder="Agregar un articulo" />
+  <!--Caja de seleccion de Prioridad-->
   <label>
-    <input type="radio" value="low" v-model="newItemPriority">
-    Baja
+    <input type="checkbox" v-model="newItemHighPriority" />
+    Alta Prioridad
   </label>
-  <label>
-    <input type="radio"  value="high" v-model="newItemPriority">
-    Alta
-  </label>
-  {{ newItemPriority == 'low' ? '🧊' : '🔥' }}
-
+  <!--Boton-->
+  <button class="btn btn-primary">
+    Salvar Articulo
+  </button>
+  </form>
+  <ul></ul>
+  
+  <!-- Lista -->
   <ul>
-    <li v-for="item in items" :key="item.id"> 💡 {{ item.label }} </li>
+    <li v-for="item in items" :key="item.id"> 📌 {{  item.label }} </li>
   </ul>
 </template>
-
+ 
 <style scoped>
-.shopping-cart-icon {
+.shopping-cart-icon{
   font-size: 2rem;
 }
 </style>
