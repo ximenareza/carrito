45ca459f83914ecd0a7fd68b7ed5f78cebab0040
ximenareza <xim3naacosta.22@gmail.com>
2024-10-02 11:47:51 -0600

    ⭐S11 Methods Create method to add item and clean input

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,22 @@
 import { ref } from 'vue';
 // Modelo
 const header = ref('App lista de compras');
+ // ----- Items
+ //------- Item-Model
 const items = ref([
   {id:'0', label: '10 bolillos'},
   {id:'1', label: '1 crema de litro'},
   {id:'2', label: '1/4 de jamon'},
   {id:'3', label: '1 nutella'}
 ]);
+// Item-Method
+const saveItem = () => {
+  items.value.push({id: items.value.length + 1, label: newItem.value});
+  // Limpia el input (Caja de texto)
+  newItem.value = '';
+
+};
+
 const newItem = ref("");
 const newItemHighPriority = ref(false);
 
@@ -18,7 +28,7 @@ const newItemHighPriority = ref(false);
    <i class="material-icons shopping-cart-icon">local_mall</i>
     {{ header }}
   </h1>
-  <form v-on:submit.prevent="items.push({id: items.length + 1, label: newItem})" class="add-item form">
+  <form v-on:submit.prevent="saveItem()">
   <input v-model="newItem" type="text" placeholder="Agregar un articulo" />
   <!--Caja de seleccion de Prioridad-->
   <label>
