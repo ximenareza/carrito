474a2f41823059db524b5c1be04ab62e54b11799
ximenareza <xim3naacosta.22@gmail.com>
2024-10-02 12:47:54 -0600

    ⭐ S12 Introduction v-if Shows and hides main form

--- src/App.vue
+++ src/App.vue
@@ -20,15 +20,28 @@ const saveItem = () => {
 
 const newItem = ref("");
 const newItemHighPriority = ref(false);
+const editing = ref(true);
+const activateEdition = (activate) => {
+  editing.value = activate;
+}
 
 </script>
  
 <template>
+  <div class="header">
   <h1>
    <i class="material-icons shopping-cart-icon">local_mall</i>
     {{ header }}
   </h1>
-  <form v-on:submit.prevent="saveItem()">
+
+<button v-if="editing" class="btn" @click="activateEdition(false)"> Cancelar</button>
+<button v-else class="btn btn-primary" @click="activateEdition(true)">Agregar Articulo</button>
+</div>
+<form
+class="add-item form"
+   v-if="editing"
+   v-on:submit.prevent="saveItem()">
+
   <input v-model="newItem" type="text" placeholder="Agregar un articulo" />
   <!--Caja de seleccion de Prioridad-->
   <label>
@@ -46,6 +59,7 @@ const newItemHighPriority = ref(false);
   <ul>
     <li v-for="item in items" :key="item.id"> 📌 {{  item.label }} </li>
   </ul>
+  <p v-if="items.length === 0">🥀 NO HAY ELEMENTOS EN LA LISTA 🥀 </p>
 </template>
  
 <style scoped>
