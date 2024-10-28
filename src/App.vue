e9191d97579ccdaa6a40ffd8dde784f9ab10672b
ximenareza <xim3naacosta.22@gmail.com>
2024-10-28 12:03:54 -0600

    ✨ FEAT: Boilerplate Creates the initial code for the proyect

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,71 @@
-<script setup>
-  import { ref } from 'vue';
-  // Modelo
-  const header = ref('App lista de compras');
-  const items = ref([
-    {id:'0', label: '10 bolillos'},
-    {id:'1', label:'1 lata frijoles'},
-    {id:'2', label:'1 Chelas'},
-    {id:'3', label:'1 Nutella'},
-  ]);
-</script>
+<script setup></script>
 
 <template>
-  <h1>
-    <i class="material-icons shopping-cart-icon">local_mall</i>
-    {{ header }}
-  </h1>
-  <ul>
-    <li v-for="item in items" :key="item.id"> 🛍️ {{ item.label }} </li>
-  </ul>
+  <header>
+    <div style ="text-align: center;">
+      <span id="logo">ITGAM BOX EXPERENCES</span> <br>
+      <img src="./assets/logo.png" alt="logo">
+    </div>
+  </header>
+
+  <div class="content">
+    <h1 class="title">Coffee Plans</h1>
+
+    <h2 class="subtitle">
+      Viajamos por el mundo para encontrar el mejor café de origen único para ti
+    </h2>
+
+    <div class="plans">
+      <div class="plan">
+        <div class="description">
+          <span class="title">
+            El Soltero
+          </span>
+        </div>
+      </div>
+      <div class="plan">
+        <div class="description">
+          <span class="title">
+            El Curioso
+          </span>
+        </div>
+      </div>
+      <div class="plan">
+        <div class="description">
+          <span class="title">
+            El adicto
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-.shopping-cart-icon {
-  font-size: 2rem;
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
 }
 </style>
