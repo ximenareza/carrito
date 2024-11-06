75bf1688fd3b771a708581efc3df0804b0cd87e1
ximenareza <xim3naacosta.22@gmail.com>
2024-11-06 12:49:10 -0600

    ⭐S03S03 Properties

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
+import {ref} from 'vue';
 import planItem from './components/plan-item.vue';
+const plans = ref (['El cafetero', 'Amigo', 'El lechero']);
 </script>
 
 <template>
@@ -18,9 +20,9 @@ import planItem from './components/plan-item.vue';
     </h2>
 
     <div class="plans">
-      <planItem></planItem>
-      <planItem></planItem>
-      <planItem></planItem>
+      <plan-item v-for="plan in plans" :key="plan" :name="plan"> </plan-item>
+      
+      
     </div>
   </div>
 </template>
--- src/components/plan-item.vue
+++ src/components/plan-item.vue
@@ -2,13 +2,25 @@
     <div class="plan">
       <div class="description">
         <span class="title">
-          El adicto
+          {{ name }}
         </span>
       </div>
     </div>
   </template>
   
   <script setup>
+
+  // Usando un macro psrs definir propiedades
+  //Son programas o funciones que están disponibles para los otros y no son necesarios importarlos 
+  //defineProps(['name','planIcon']); //Para establecer nombres de propiedades siempre usar mayucula en la primer letra de la segunda palabra
+  //El sigueinte es un ejemplo de esquema definit¿do
+  defineProps({
+    name: {
+    type: String,
+    required: true
+  },
+  });
+
   
   </script>
   
