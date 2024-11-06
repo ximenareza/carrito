2bbe0da8f0017af61d2c7f1cdcf6560389f6ca8e
ximenareza <xim3naacosta.22@gmail.com>
2024-11-06 08:37:55 -0600

    ⭐FEAT: Add plan-item in Components Sesión 3

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
-<script setup></script>
+<script setup>
+import planItem from './components/plan-item.vue';
+</script>
 
 <template>
   <header>
-    <div style ="text-align: center;">
-      <span id="logo">ITGAM BOX EXPERENCES</span> <br>
+    <div>
+      <span id="logo">Liquid Gold Box</span>
       <img src="./assets/logo.png" alt="logo">
     </div>
   </header>
@@ -16,56 +18,12 @@
     </h2>
 
     <div class="plans">
-      <div class="plan">
-        <div class="description">
-          <span class="title">
-            El Soltero
-          </span>
-        </div>
-      </div>
-      <div class="plan">
-        <div class="description">
-          <span class="title">
-            El Curioso
-          </span>
-        </div>
-      </div>
-      <div class="plan">
-        <div class="description">
-          <span class="title">
-            El adicto
-          </span>
-        </div>
-      </div>
+      <planItem></planItem>
+      <planItem></planItem>
+      <planItem></planItem>
     </div>
   </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/plan-item.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="plan">
+      <div class="description">
+        <span class="title">
+          El adicto
+        </span>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  
+  </script>
+  
+  <style scoped>
+  
+  </style>
