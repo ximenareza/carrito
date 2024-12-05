127390a240690b566c1e199ad477220dd4427d24
ximenareza <xim3naacosta.22@gmail.com>
2024-12-04 23:18:36 -0600

    🚧 Commit 1 Slots

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,10 @@
 <script setup>
-import PlanPicker from './components/PlanPicker.vue';
-import { ref } from 'vue';
-
-const showPlans = ref(false);
+import fancyButton from './components/FancyButton.vue';
 </script>
 
 <template>
-  <header>
-    <div>
-      <span id="logo">Liquid Gold Box</span>
-      <img src="./assets/logo.png" alt="logo">
-    </div>
-  </header>
-
   <div class="content">
-    <h1 class="title">Coffee Plans</h1>
-
-    <h2 class="subtitle">
-      Viajamos por el mundo para encontrar el mejor café de origen único para ti
-    </h2>
-
-    <label>
-    <input type="checkbox" v-model="showPlans">
-    Mostrar selector de planes 
-
-    </label>
-    
-
-    <!--TODO INCLUDE PLAN-PICKER COMPONENT-->
-    <PlanPicker v-if="showPlans" />
-
-
-    
+    <fancy-button />
   </div>
 </template>
 
---
+++ src/components/FancyButton.vue
@@ -0,0 +1,10 @@
+<template>
+    <button>Hazme Clic!</button>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style scoped>
+  
+  </style>
