f7fd289e17a27fca03508607836524da0b1392fa
ximenareza <xim3naacosta.22@gmail.com>
2024-12-05 00:02:03 -0600

    ⭐ Slots concluido

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,12 @@ import fancyButton from './components/FancyButton.vue';
 
 <template>
   <div class="content">
-    <fancy-button />
+    <fancy-button>
+      <template #icon="{ hover }">
+        {{ hover ? "🌟" : "⭐"}}
+      </template>
+      Hazme Clic
+    </fancy-button>
   </div>
 </template>
 
--- src/components/FancyButton.vue
+++ src/components/FancyButton.vue
@@ -1,8 +1,15 @@
 <template>
-    <button>Hazme Clic!</button>
+    <button 
+      @mouseenter="hover = true"
+      @mouseleave="hover = false"> 
+      <slot name="icon" :hover="hover">➡️</slot>
+      <slot>Submit</slot>
+    </button>
   </template>
   
   <script setup>
+  import { ref } from 'vue';
+  const hover = ref(false);
   </script>
   
   <style scoped>
